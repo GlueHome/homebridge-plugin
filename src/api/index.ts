cdda9938c909f66732656ecd5109f5071018932d
Marco Talento <marco@gluehome.com>
2020-12-06 12:40:05 +0000

    fix file name

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,3 @@
 export { Lock, EventType, CreateLockOperation, LockOperation, LockOperationType } from "./lock";
-export { Hub } from "./Hub"
+export { Hub } from "./hub"
 export { GlueApi } from "./client"
