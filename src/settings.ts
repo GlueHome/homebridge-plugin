e85702e32887ba18ae82405e8ae2bb0cc1dc804d
Stavros Kafouros <stavros@gluehome.com>
2021-01-26 19:34:14 +0000

    General fixes
    Add BasicAuth scheme alternative through configuration.
    Graceful handling of 503 (lock busy) errors.
    Increase polling frequency during a lock operation.

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,14 @@
+import { platform, release } from 'os';
+
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
 export const PLATFORM_NAME = 'GlueHomebridge';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+export const VERSION = require('../package.json').version;
+
+export const OS_VERSION = `${platform()} ${release()}`;
 
 /**
  * This must match the name of your plugin as defined the package.json
